c00087cd85d0088222ebe588a23d41f8a3ffc8cf
Ryan Bishop <rdbishop06@gmail.com>
2020-02-08 17:43:32 -0600

    feat: home page view

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -4,7 +4,7 @@ from .views import *
 app_name = "libraryapp"
 
 urlpatterns = [
-    path('', book_list, name='home'),
+    path('', home, name='home'),
     path('books/', book_list, name='books'),
     path('librarians/', librarian_list, name='librarians'),
     path('libraries/', library_list, name='libraries'),
