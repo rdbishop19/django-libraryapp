9e8b67c8c68c66545012af899a4032eb1f77bbcc
Ryan Bishop <rdbishop06@gmail.com>
2020-02-08 21:40:16 -0600

    feat: create new book form

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('books/', book_list, name='books'),
     path('librarians/', librarian_list, name='librarians'),
     path('libraries/', library_list, name='libraries'),
+    path('book/form', book_form, name='book_form'),
 ]
